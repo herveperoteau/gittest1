a5f4509079f2327fdf56b57a3f5b340cc6953e0d
Herve Peroteau <herve.peroteau@gmail.com>
2013-04-27 18:06:13 +0200

    Initial Commit

---
+++ GitExampleSimple/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GitExampleSimple
+//
+//  Created by Hervé PEROTEAU on 27/04/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ GitExampleSimple/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitExampleSimple
+//
+//  Created by Hervé PEROTEAU on 27/04/13.
+//  Copyright (c) 2013 Hervé PEROTEAU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
